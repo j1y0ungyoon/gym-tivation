ed1e06043bed004c3471c5ece6c9de07d4a12b1a
SeongYoungJu <xtotoroz3@gmail.com>
2023-02-12 17:41:55 +0900

    Feat: apply updating recruitment post
    - 동료 모집 게시글 상세 페이지에서 수정 기능을 추가
    - 기존 RecruitPost에 있었던 삭제 기능을 상세 페이지에서 작동하도록 구현
    
    by @SeongYeongJu

--- pages/recruitDetail/[...params].tsx
+++ pages/recruitDetail/[...params].tsx
@@ -1,37 +1,157 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { editRecruitPost, deleteRecruitPost } from '../api/api';
+import { EditRecruitPostParameterType } from '../type';
 
 const RecruitDetail = ({ params }: any) => {
-  const [detailPost, setDetailPost] = useState<object>();
   const router = useRouter();
-
-  // getServerSideProps를 이용하면 router.push를 이용해 url로 데이터를 전달해도
-  // 새로고침 시 해당 페이지에서 데이터가 사라지지 않는다.
-  // params로 데이터를 받아와서 활용하면 된다.
   const [id, title, content, createdAt] = params;
 
-  console.log(id);
-  console.log(title);
-  console.log(content);
-  console.log(createdAt);
+  const [changeForm, setChangeForm] = useState(false);
+  const [editTitle, setEditTitle] = useState(title);
+  const [editContent, setEditContent] = useState(content);
 
-  // useEffect(() => {
-  //   const { pid } = router.query;
-  //   const getDetailPost = async () => {
-  //     if (typeof pid === 'string') {
-  //       const docRef = doc(dbService, 'recruitments', pid);
-  //       const docSnap = await getDoc(docRef);
-  //       return docSnap.data();
-  //     }
-  //   };
-  // }, []);
+  // 게시글 수정 useMutation
+  const { isLoading: isEditting, mutate: reviseRecruitPost } = useMutation(
+    ['editRecruitPost', id],
+    (body: EditRecruitPostParameterType) => editRecruitPost(body),
+    {
+      onSuccess: () => {
+        console.log('수정 성공');
+      },
+      onError: (err) => {
+        console.log('수정 실패:', err);
+      },
+    },
+  );
+
+  // 게시글 삭제 useMutation
+  const { isLoading: isDeleting, mutate: removeRecruitPost } = useMutation(
+    ['deleteRecruitPost', id],
+    (body: string) => deleteRecruitPost(body),
+    {
+      onSuccess: () => {
+        console.log('삭제성공');
+      },
+      onError: (err) => {
+        console.log('삭제 실패:', err);
+      },
+    },
+  );
+
+  // 게시글 삭제 클릭 이벤트
+  const onClickDeletePost = async () => {
+    const answer = confirm('정말 삭제하시겠습니까?');
+
+    if (answer) {
+      try {
+        await removeRecruitPost(id);
+        router.push('/mapBoard');
+      } catch (error) {
+        console.log('에러입니다', error);
+      }
+    } else {
+      return;
+    }
+  };
+
+  // 게시글 수정을 위한 input open
+  const onClickChangeForm = () => {
+    setChangeForm(!changeForm);
+  };
+
+  const onChangeEditTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+
+    setEditTitle(value);
+  };
+
+  const onChangeEditContent = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const {
+      currentTarget: { value },
+    } = event;
+
+    setEditContent(value);
+  };
+
+  // 게시글 수정
+  const onSubmitEdittedPost = async (
+    event: React.FormEvent<HTMLFormElement>,
+  ) => {
+    event.preventDefault();
+
+    if (!editTitle) {
+      alert('제목을 작성해주세요!');
+      return;
+    }
+
+    if (!editContent) {
+      alert('내용을 작성해주세요!');
+      return;
+    }
+
+    let edittedRecruitPost = {};
+
+    Object.assign(edittedRecruitPost, {
+      title: editTitle,
+      content: editContent,
+    });
+
+    try {
+      await reviseRecruitPost({ recruitPostId: id, edittedRecruitPost });
+      setEditTitle('');
+      setEditContent('');
+      setChangeForm(false);
+
+      // detail에서 변경된 정보를 바로 불러오지 못하기 때문에 mapBoard로 경로 이동 시켜줘야 함.
+      // useQuery를 이용하면 경로 이동을 안해도 될 것이므로 사용자가 수정하자마자 수정 내용을 바로 확인할 수 있게 가능할 것 같음.
+      // 하지만 통신 비용이 더 들 수 있다.
+      router.push('/mapBoard');
+    } catch (error) {
+      console.log('에러입니다', error);
+    }
+  };
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <h4>{content}</h4>
-      <span>{createdAt}</span>
-    </div>
+    <>
+      {isEditting ? (
+        <div>게시물을 수정중입니다</div>
+      ) : isDeleting ? (
+        <div>게시물을 삭제중입니다</div>
+      ) : (
+        <>
+          {changeForm ? (
+            <div>
+              <form onSubmit={onSubmitEdittedPost}>
+                <input defaultValue={title} onChange={onChangeEditTitle} />{' '}
+                <br />
+                <textarea
+                  defaultValue={content}
+                  onChange={onChangeEditContent}
+                />{' '}
+                <br />
+                <span>{createdAt}</span> <br />
+                <button>수정 완료</button>
+              </form>
+              <button onClick={onClickChangeForm}>취소</button>
+            </div>
+          ) : (
+            <div>
+              <h3>{title}</h3>
+              <h4>{content}</h4>
+              <span>{createdAt}</span> <br />
+              <button onClick={onClickChangeForm}>수정</button>
+              <button onClick={onClickDeletePost}>삭제</button>
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
@@ -45,10 +165,3 @@ export function getServerSideProps({ params: { params } }: any) {
 }
 
 export default RecruitDetail;
-
-// 1. RecruitPost 컴포넌트에서 RecruitDetail에 파람스로 post.id를 보내고
-// 2. post.id를 이용해서 getDoc 한 후,
-// 3. input, textarea 태그 defaultValue에 기존 텍스트를 준다.
-// 4. '삭제하기' 버튼을 누르면 삭제가 되어야 한다.
-// 5. '수정하기' 버튼을 누르면 기존 텍스트는 유지가 된 상태로 수정이 진행되어야 한다.
-// 6. '수정 완료' 버튼을 누르면 update가 완료되고 다시 detail 페이지가 보인다.
