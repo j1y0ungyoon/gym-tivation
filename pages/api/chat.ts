f6862f31e42c5358feea1b7a3692fd8e9d748d50
GoMyamMii <sun302700@naver.com>
2023-02-20 00:48:40 +0900

    Feat : apply socketServer
    채팅 소켓 서버 구현
    
    by @GoMyamMii

---
+++ pages/api/chat.ts
@@ -0,0 +1,29 @@
+import { Server as HTTPServer } from 'http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { Socket as NetSocket } from 'net';
+import type { Server as IOServerType } from 'socket.io';
+import { Server as IOServer } from 'socket.io';
+
+// 타입 지정 해주기 io, server, socket
+interface SocketServer extends HTTPServer {
+  io?: IOServerType | undefined;
+}
+interface SocketWithIO extends NetSocket {
+  server: SocketServer;
+}
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO;
+}
+
+const socketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
+  if (res.socket.server.io) {
+    console.log('이미 소켓서버가 작동중입니다.');
+  } else {
+    console.log('소켓 서버 초기화');
+    const io = new IOServer(res.socket.server);
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+export default socketHandler;
