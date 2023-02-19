fbf64454a0c7633c3c2de6a03304010f772c2257
GoMyamMii <sun302700@naver.com>
2023-02-20 02:01:49 +0900

    Feat : apply chat to all
    전체채팅 기능 구현
    
    by @GoMyamMii

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -22,6 +22,19 @@ const socketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
     console.log('소켓 서버 초기화');
     const io = new IOServer(res.socket.server);
     res.socket.server.io = io;
+
+    io.on('connection', (socket) => {
+      console.log('클라이언트 접속 완료');
+      socket.on('disconnected', () => {
+        console.log('클라이언트 접속 종료');
+      });
+
+      // 전체 채팅 받는곳
+      socket.on('chat', (data) => {
+        console.log('채팅 받음', data);
+        io.emit('chat', data);
+      });
+    });
   }
   res.end();
 };
--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,7 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Socket } from 'socket.io';
 import io from 'socket.io-client';
+import { nanoid } from 'nanoid';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+
+type ChatLog = {
+  id: number;
+  msg: string;
+  username: string;
+};
 
 const Chat = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [chatLogs, setChatLogs] = useState<ChatLog[]>([
+    { id: 1, msg: '어서오세요!', username: 'username' },
+  ]);
+  const [socket, setSocket] = useState<Socket<DefaultEventsMap> | null>(null);
+
   // useEffect 로 처음 접속시 소켓서버 접속
   useEffect(() => {
     // socket.io 접속 함수
@@ -9,15 +24,69 @@ const Chat = () => {
       await fetch('/api/chat');
       const socket = io();
 
+      setSocket(socket as any);
+
+      // 초기 연결
       socket.on('connect', () => {
         console.log('연결성공!');
+      });
+
+      // "chat" 이름으로 받은 chatLogs(채팅내용들) 서버에서 받아옴
+      socket.on('chat', (chatLog: any) => {
+        setChatLogs((prev) => [...prev, chatLog]);
       });
     };
     // 소켓서버 접속함수 실행
     connectToSocket();
+
+    // 소켓 여러번 뚫리는거 방지용
+    return () => {
+      socket?.disconnect();
+    };
   }, []);
 
-  return <div>Chatting</div>;
+  // 채팅 전송시 실행 함수
+  const postChat = (e: React.KeyboardEvent<EventTarget>) => {
+    if (e.key !== 'Enter') return;
+    if (inputValue === '') return;
+
+    const chatLog = {
+      id: nanoid(),
+      msg: (e.target as any).value,
+      username: 'testUser',
+    };
+
+    // "chat" 이름으로 chatLog(채팅내용) 서버로 올려줌
+    socket?.emit('chat', chatLog);
+    setInputValue('');
+  };
+
+  const onChangeInputVlaue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Chatting</h1>
+
+      <div>
+        {chatLogs.map((chatLog) => (
+          <div key={chatLog.id}>
+            <p>
+              {chatLog.username} : {chatLog.msg}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <input
+        type="text"
+        onKeyPress={postChat}
+        value={inputValue}
+        onChange={onChangeInputVlaue}
+      />
+    </div>
+  );
 };
 
 export default Chat;
