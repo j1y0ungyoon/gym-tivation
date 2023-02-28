dfa8a9af849581bad66afe7917a143fbd7ba7166
GoMyamMii <sun302700@naver.com>
2023-02-28 20:58:10 +0900

    Feat : apply theme
    - theme 파일 추가
    
    by @GoMyamMii

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '@/components/Header';
 import SideNav from '@/components/SideNav';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { authService } from '@/firebase';
 import { useState, useEffect } from 'react';
+import { theme } from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -22,11 +23,13 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Header isLoggedIn={isLoggedIn} />
-      <Layout>
-        <SideNav isLoggedIn={isLoggedIn} />
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Header isLoggedIn={isLoggedIn} />
+        <Layout>
+          <SideNav isLoggedIn={isLoggedIn} />
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
