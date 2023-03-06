4a4cba939bcec831ed6e153413e24b19c3217ba9
GoMyamMii <sun302700@naver.com>
2023-03-06 12:31:08 +0900

    Refactor : refactor main slide, ui
    - 메인 페이지의 ui 변경
    - 메인 페이지 참여 동료 수 구현
    - nav 의 메뉴 svg 아이콘으로 변경
    
    by @GoMyamMii

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { authService, dbService } from '@/firebase';
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
+import {
+  collection,
+  getCountFromServer,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from 'firebase/firestore';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -15,6 +22,7 @@ const Home = () => {
   const user = authService.currentUser;
 
   const [mainImgs, setMainImgs] = useState<any>([]);
+  const [userCount, setUserCount] = useState<number>();
 
   useEffect(() => {
     const getGallery = async () => {
@@ -32,6 +40,15 @@ const Home = () => {
 
       setMainImgs(gallery);
     };
+    const getProfile = async () => {
+      const profileCollection = collection(dbService, 'profile');
+      const snapshot = await getCountFromServer(profileCollection);
+      const profileCount = snapshot.data().count;
+      console.log('count', profileCount);
+      setUserCount(profileCount);
+    };
+
+    getProfile();
     getGallery();
   }, []);
 
@@ -44,20 +61,37 @@ const Home = () => {
     autoplaySpeed: 2000,
     slidesToShow: 3,
     slideToScroll: 1,
-    centerPadding: '60px',
+    centerPadding: '0',
     speed: 500,
-    arrows: false,
+    nextArrow: (
+      <ArrowR>
+        <SVG src={'/assets/icons/next.svg'} />
+      </ArrowR>
+    ),
+    prevArrow: (
+      <ArrowL>
+        <SVG src={'/assets/icons/prev.svg'} />
+      </ArrowL>
+    ),
   };
 
   return (
     <HomeWrapper>
       <HomeContainer>
-        <TitleText>현재 함께 운동중인 동료들 2683명!</TitleText>
-        <SliderContainer {...settings}>
-          {mainImgs?.map((mainImg: string) => {
-            return <Img key={mainImg} mainImg={mainImg} />;
-          })}
-        </SliderContainer>
+        <SubTitleText>
+          운동 동기부여 어렵지 않아요! GYMTIVATION과 함께해봐요!
+        </SubTitleText>
+        <TitleText>
+          {userCount}명의 동료들이 짐티베이션에 참여하고 있습니다!
+        </TitleText>
+        <DIV>
+          <SliderContainer {...settings}>
+            {mainImgs?.map((mainImg: string) => {
+              return <Img key={mainImg} mainImg={mainImg} />;
+            })}
+          </SliderContainer>
+          <TitleSvg src={'/assets/icons/title.svg'} />
+        </DIV>
       </HomeContainer>
     </HomeWrapper>
   );
@@ -69,16 +103,28 @@ const HomeWrapper = styled.main`
 
 const HomeContainer = styled.div`
   ${({ theme }) => theme.mainLayout.container}
+  height: calc(100vh - 100px);
 `;
 
+const SubTitleText = styled.h1`
+  height: 24px;
+  margin-top: 60px;
+  font-size: 20px;
+  text-align: center;
+`;
 const TitleText = styled.h1`
   height: 60px;
-  margin-top: 50px;
-  font-size: 3rem;
+  margin-top: 10px;
+  font-size: 30px;
   font-weight: bold;
   text-align: center;
 `;
-
+const DIV = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Img = styled.div<ImgBoxProps>`
   background-image: ${(props) => `url(${props.mainImg})`};
   background-size: cover;
@@ -87,10 +133,20 @@ const Img = styled.div<ImgBoxProps>`
 
 const SliderContainer = styled(Slider)`
   width: 100%;
-  height: calc(100% - 120px);
+  height: calc(100% - 320px);
+  min-height: 620px;
+
+  position: relative;
+
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
 
   .slick-list {
     width: 100%;
+    min-height: 600px;
     height: 100%;
   }
 
@@ -98,25 +154,50 @@ const SliderContainer = styled(Slider)`
   }
 
   .slick-slide {
-    min-height: 740px;
-    height: calc(100vh - 240px);
+    min-height: 600px;
+    height: calc(100vh - 440px);
     display: flex;
     align-items: center;
   }
   .slick-slide div {
-    width: 100%;
+    width: 380px;
+    height: 380px;
   }
   .slick-slide div div {
-    height: 500px;
+    width: 100%;
+    height: 100%;
     border-radius: 500px;
     object-fit: cover;
   }
   .slick-center div div {
-    height: 500px;
+    width: 100%;
+    height: 100%;
     border-radius: 500px;
     transition: all 300ms ease;
     transform: scale(1.4);
   }
 `;
+const TitleSvg = styled.img`
+  position: absolute;
+  bottom: 40px;
+`;
 
+const ArrowR = styled.div`
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  right: 48px;
+  z-index: 99;
+`;
+const ArrowL = styled.div`
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  left: 48px;
+  z-index: 99;
+`;
+const SVG = styled.img`
+  width: 40px;
+  height: 40px;
+`;
 export default Home;
