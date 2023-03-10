8e48ea86b6dfa115e47308a4181fb66105086385
park74441 <118245133+park74441@users.noreply.github.com>
2023-03-10 16:21:36 +0900

    fix: modal 쉐도우 추가 by @park74441

--- pages/myPage/[...params].tsx
+++ pages/myPage/[...params].tsx
@@ -411,6 +411,7 @@ const FollowModal = styled.div`
   top: 50%;
   left: 50%;
   border-radius: 15px;
+  box-shadow: -2px 2px 0px 0px #000000;
   transform: translate(-50%, -50%) !important;
   padding-top: 1.5rem;
   background-color: #fffcf3;
